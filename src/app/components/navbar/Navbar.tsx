8045228443b39ff4093d3d8bd24f4ae3c32762df
ilhan korkmaz <korkmazz5625@gmail.com>
2024-05-01 23:31:52 +0300

    Navbar component added

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import styles from "./Navbar.module.css";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+export default function Navbar() {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.menu}>
+          <li className={styles.menu_item}>SHOP</li>
+          <li className={styles.menu_item}>HOW IT WORKS</li>
+          <li className={styles.menu_item}>FAQ</li>
+          <li className={styles.menu_item}>CONTACT</li>
+          <li className={styles.shoppingCard}>
+            <MdOutlineShoppingCart size={18} />
+            <div className={styles.badge}>2</div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
